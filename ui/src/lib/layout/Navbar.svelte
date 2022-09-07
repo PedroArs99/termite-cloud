ee858da059e8816a224a921c8f9d97c1c886599c
Pedro Arenas <pedroars99@icloud.com>
2022-09-07 21:00:57 +0200

    [Home-Ui] Add Navbar Component

---
+++ ui/src/lib/layout/Navbar.svelte
@@ -0,0 +1,9 @@
+<div class="navbar bg-base-100">
+	<div class="navbar-start"></div>
+	<div class="navbar-center">
+		<a href="/" class="btn btn-ghost normal-case text-xl">
+			<img src="/favicon.png" alt="Termite Home Logo" width="64px">
+		</a>
+	</div>
+	<div class="navbar-end"></div>
+</div>
