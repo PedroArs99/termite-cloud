10118193be26c43879bed4194affe2e8cf41a17d
Pedro Arenas <pedroars99@icloud.com>
2022-09-07 20:45:54 +0200

    [Home-Ui] Init DaisyUi

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -6,4 +6,16 @@
 	<meta name="description" content="Termite Cloud App - Home Control" />
 </svelte:head>
 
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<div class="mockup-phone">
+	<div class="camera" />
+	<div class="display">
+		<div class="artboard artboard-demo phone-1">
+			<button class="btn">Button</button>
+			<button class="btn btn-primary">Button</button>
+			<button class="btn btn-secondary">Button</button>
+			<button class="btn btn-accent">Button</button>
+			<button class="btn btn-ghost">Button</button>
+			<button class="btn btn-link">Button</button>
+		</div>
+	</div>
+</div>
--- ui/tailwind.config.cjs
+++ ui/tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
+  daisyui : {
+    themes: ["dracula"]
+  },
+  plugins: [
+    require("daisyui")
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
 }
