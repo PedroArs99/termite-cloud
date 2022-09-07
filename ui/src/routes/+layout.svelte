e817fdda5c56266c65114b471b23befefdf2a614
Pedro Arenas <pedroars99@icloud.com>
2022-09-07 21:01:24 +0200

    [Home-Ui] Display Navbar on layout

--- ui/src/routes/+layout.svelte
+++ ui/src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import '../app.css';
+	import Navbar from '../lib/layout/Navbar.svelte';
 </script>
+
+<Navbar />
 
 <main>
 	<slot />
--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -6,16 +6,3 @@
 	<meta name="description" content="Termite Cloud App - Home Control" />
 </svelte:head>
 
-<div class="mockup-phone">
-	<div class="camera" />
-	<div class="display">
-		<div class="artboard artboard-demo phone-1">
-			<button class="btn">Button</button>
-			<button class="btn btn-primary">Button</button>
-			<button class="btn btn-secondary">Button</button>
-			<button class="btn btn-accent">Button</button>
-			<button class="btn btn-ghost">Button</button>
-			<button class="btn btn-link">Button</button>
-		</div>
-	</div>
-</div>
