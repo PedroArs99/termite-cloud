9a3014d2135b47648f1bcf125749c584cd8f943f
Pedro Arenas <pedroars99@icloud.com>
2022-09-08 22:02:32 +0200

    [Home-Api] Reorder HomeStateModule imports

--- api/src/modules/home/state/home.state.module.ts
+++ api/src/modules/home/state/home.state.module.ts
@@ -2,19 +2,27 @@ import { CacheModule, Module } from "@nestjs/common";
 import { CacheManagerAdapter } from "./adapters/cacheManager.adapter";
 import { HomeStateMqttController } from "./adapters/home.state.mqtt.controller";
 import { CacheManagerPort } from "./application/ports/CacheManager.port";
+import { CqrsModule } from "@nestjs/cqrs";
+import { SaveHomeStateCommandHandler } from "./application/commands/save/saveHomeState.handler";
+import { HomeStateRestController } from "./adapters/home.state.rest.controller";
+import { GetHomeStateHandler } from "./application/queries/getHomeState.handler";
 
+const commandHandlers = [ SaveHomeStateCommandHandler ]
+const controllers = [ HomeStateMqttController, HomeStateRestController ];
+const queryHandlers = [ GetHomeStateHandler ]
 @Module({
-    controllers: [
-        HomeStateMqttController
-    ],
+    controllers,
     imports: [
-        CacheModule.register()
+        CacheModule.register(),
+        CqrsModule
     ],
     providers: [
         {
             provide: CacheManagerPort,
             useClass: CacheManagerAdapter
-        }
+        },
+        ...commandHandlers,
+        ...queryHandlers
     ]
 })
 export class HomeStateModule{}
