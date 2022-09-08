40dab95b68b93a73fb01483340ff1beadcffcb5d
Pedro Arenas <pedroars99@icloud.com>
2022-09-08 18:55:42 +0200

    [Home-Api] Remove mqtt module from home

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-import { MqttAdapterModule } from './adapters/mqtt/mqtt.adapter.module';
 
 @Module({
   imports: [
-    MqttAdapterModule,
     // Seve UI Static files
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'assets'),
