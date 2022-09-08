d625c335e975a9b72f63acabf9d836992a8a7f2b
Pedro Arenas <pedroars99@icloud.com>
2022-09-08 22:28:55 +0200

    [Home-Ui] Add HomeStatePage  component

---
+++ ui/src/lib/modules/home/state/components/HomeStatePage.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	import { fetchHomeState, homeState } from '../home.state.store';
+
+	onMount(() => fetchHomeState());
+</script>
+
+<h1>Home Store : {$homeState} </h1>
