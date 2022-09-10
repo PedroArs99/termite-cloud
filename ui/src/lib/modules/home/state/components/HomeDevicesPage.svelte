a9bb6f36d1201b0d039a92770c84d84c518577fa
Pedro Arenas <pedroars99@icloud.com>
2022-09-11 00:36:53 +0200

    [Home-Ui] Add Device display component

---
+++ ui/src/lib/modules/home/state/components/HomeDevicesPage.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	import { fetchHomeState, homeState } from '../home.state.store';
+	import type { Device } from '../models/device/Device.model';
+	import BulbImage from './devices/bulb/BulbImage.svelte';
+
+	let devices: Device[];
+
+	$: devices = $homeState.devices;
+
+	onMount(() => fetchHomeState());
+</script>
+
+{#each devices as device}
+	<div class="card card-xs">
+		<div class="card-body">
+			<BulbImage clazz="border fill-primary rounded-full shadow-2xl p-3" width="64px" />
+			<h1>{device.friendlyName}</h1>
+		</div>
+	</div>
+{/each}
+
+<style lang="postcss">
+	.card-body {
+		display: flex;
+		align-items: center;
+	}
+</style>
