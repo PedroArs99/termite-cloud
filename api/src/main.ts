ec4d68a586f74152480116d38332ce451c6238dd
Pedro Arenas <arenaspeerz3c@outlook.es>
2022-09-13 22:44:31 +0200

    [Home-Api] Fix envs

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,17 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  // Init App context
   const app = await NestFactory.create(AppModule, {
     logger: ['log'],
   });
+  
+  // Inject Env
+  const configService = app.get<ConfigService>(ConfigService);
+  const envs = {
+    mqtt_url: configService.get("MQTT_URL"),
+    mqtt_username: configService.get("MQTT_USERNAME"),
+    mqtt_password: configService.get("MQTT_PASSWORD"),
+  }
+
   app.connectMicroservice({
     transport: Transport.MQTT,
     options: {
-      url: process.env.MQTT_URL,
-      username: process.env.MQTT_USERNAME,
-      password: process.env.MQTT_PASSWORD,
+      url: envs.mqtt_url,
+      username: envs.mqtt_username,
+      password: envs.mqtt_password,
     },
   });
 
