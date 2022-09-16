5779cb8f4aeb5cc43665c1fcfe374aa5b1441df0
Pedro Arenas <pedroars99@icloud.com>
2022-09-17 01:11:36 +0200

    [Home-Ui] Add Icon component

---
+++ ui/src/lib/utils/Icon.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Fa from 'svelte-fa';
+	import { faSignal } from '@fortawesome/free-solid-svg-icons';
+
+    export let icon: "signal";
+
+    let icons = {
+        "signal": faSignal
+    }
+</script>
+
+<Fa icon={icons[icon]} />
