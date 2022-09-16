c6620caed8b15a97adc46d65a5ca94dc36365359
Pedro Arenas <pedroars99@icloud.com>
2022-09-17 01:01:44 +0200

    [Home-Ui] Add Device component

---
+++ ui/src/lib/modules/home/state/components/devices/Device.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { Device } from '../../models/device/Device.model';
+	import BulbImage from './bulb/BulbImage.svelte';
+
+	export let device: Device;
+</script>
+
+<div class="card">
+	<div class="card-body">
+		<BulbImage clazz="border fill-primary rounded-full shadow-2xl p-3" width="64px" />
+		<h1 class="text-center">{device.friendlyName}</h1>
+	</div>
+</div>
+
+<style lang="postcss">
+	.card-body {
+		display: flex;
+		align-items: center;
+	}
+</style>
