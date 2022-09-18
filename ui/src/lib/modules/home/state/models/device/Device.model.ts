5c89e020f128924c191d408fc8180f2164fa85e2
Pedro Arenas <pedroars99@icloud.com>
2022-09-18 17:33:26 +0200

    [Home-Ui] Add DeviceState interface

--- ui/src/lib/modules/home/state/models/device/Device.model.ts
+++ ui/src/lib/modules/home/state/models/device/Device.model.ts
@@ -1,6 +1,8 @@
+import type { DeviceState } from "./DeviceState.model";
+
 export interface Device {
     friendlyName: string;
     ieeeAddress: string;
     type: string;
-    state: "ON" | "OFF" | "undefined";
+    state: DeviceState | "undefined";
 } 
