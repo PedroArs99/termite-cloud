62961882585ce08eeaf80cabd7fffc70444b1144
Pedro Arenas <pedroars99@icloud.com>
2022-09-18 16:24:41 +0200

    [Home-Ui] Refactor Blinking & Add Light ON/OFF effects

--- ui/src/lib/modules/home/state/components/devices/Device.svelte
+++ ui/src/lib/modules/home/state/components/devices/Device.svelte
@@ -4,29 +4,33 @@
 	import type { Device } from '../../models/device/Device.model';
 
 	export let device: Device;
+	let deviceDomNode: HTMLElement | null;
+	let deviceIconColor: string;
 
-	function makeDeviceBlink(deviceDomNode: HTMLElement, toggler: number) {
-		if(toggler % 2 === 0){
-			deviceDomNode.classList.add("bg-error", "border-error");
-		} else {
-			deviceDomNode.classList.remove("bg-error", "border-error");
-		}
+	function makeDeviceBlink() {
+		let toggler = true;
+		let intervalId = setInterval(() => {
+			if (toggler) {
+				deviceDomNode?.classList.add('bg-error', 'border-error');
+			} else {
+				deviceDomNode?.classList.remove('bg-error', 'border-error');
+			}
+
+			toggler = !toggler
+		}, 1000);
 	}
 
 	onMount(() => {
-		const deviceDomNode = document.getElementById(`${device.friendlyName}`);
-		
-		let toggler = 0;
-		let intervalId = setInterval(() => { 
-			makeDeviceBlink(deviceDomNode!, toggler);
-			toggler++;
-		 }, 1000)
+		deviceDomNode = document.getElementById(`${device.friendlyName}`);
 	});
+
+	$: if(device.state === "undefined") makeDeviceBlink();
+	$: deviceIconColor = device.state === "ON" ? "#efc070" : "#080703";
 </script>
 
 <div class="card">
 	<div id={device.friendlyName} class="card-body">
-		<Icon clazz="border rounded-full p-3" icon="lightbulb" size="5x" />
+		<Icon clazz="border rounded-full p-3" icon="lightbulb" size="5x" color={deviceIconColor}  />
 		<h1 class="text-center">{device.friendlyName}</h1>
 	</div>
 </div>
