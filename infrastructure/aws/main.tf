bec42cd0d3983ffc3e0400868b192c5dadd59938
Pedro Arenas <pedroars99@icloud.com>
2022-10-06 21:10:49 +0200

    [Infrastructure] Add credentials and variables file

--- infrastructure/aws/main.tf
+++ infrastructure/aws/main.tf
@@ -12,20 +12,10 @@ variable "region" {
   type = string
   default = "eu-central-1"
 }
-
-variable "credentials" {
-  type = object({
-    access_key = string
-    secret_key = string
-  })
-
-  sensitive = true
-}
 // ---
 
 provider "aws" {
   region = var.region
-  access_key = var.credentials.access_key
-  secret_key = var.credentials.secret_key
+  shared_credentials_files = [ ".credentials" ]
 }
 
