b9b0a119a5847b8d6e15572a85ad7392de7ef301
Pedro Arenas <pedroars99@icloud.com>
2022-10-08 16:48:28 +0200

    Add Aws Key-Pair

--- aws/main.tf
+++ aws/main.tf
@@ -68,15 +68,14 @@ resource "aws_route_table_association" "tch_rt_association" {
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "Allow SSH inbound traffic"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = aws_vpc.tch_vpc.id
 
   ingress {
     description      = "SSH from VPC"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+    cidr_blocks      = [aws_vpc.tch_vpc.cidr_block]
   }
 
   egress {
@@ -95,15 +94,14 @@ resource "aws_security_group" "allow_ssh" {
 resource "aws_security_group" "allow_http" {
   name        = "allow_http"
   description = "Allow HTTP inbound traffic"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = aws_vpc.tch_vpc.id
 
   ingress {
     description      = "HTTP from VPC"
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+    cidr_blocks      = [aws_vpc.tch_vpc.cidr_block]
   }
 
   egress {
@@ -117,5 +115,10 @@ resource "aws_security_group" "allow_http" {
   tags = {
     Name = "allow_http"
   }
+}
+
+resource "aws_key_pair" "tch_key_pair" {
+  key_name = "tch_key"
+  public_key = file("~/.ssh/tch_rsa.pub")
 }
 // ---
