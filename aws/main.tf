10ec5655b1dbef6e0638af8f30caee55fb9d2958
Pedro Arenas <pedroars99@icloud.com>
2022-10-08 16:27:51 +0200

    Add Aws Subnet

--- aws/main.tf
+++ aws/main.tf
@@ -28,4 +28,15 @@ resource "aws_vpc" "tch_vpc" {
     Name = "tch"
   }
 }
+
+resource "aws_subnet" "tch_subnet" {
+  vpc_id                  = aws_vpc.tch_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "tch"
+  }
+}
+
 // ---
