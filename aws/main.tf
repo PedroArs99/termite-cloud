396f5640d1a6f1ddd98923f82423863675038081
Pedro Arenas <48551390+PedroArs99@users.noreply.github.com>
2022-10-09 20:09:33 +0200

    Declare AWS Infrastructure with Terraform
    * Add Aws Subnet
    
    * Add Aws Internet Gateway
    
    * Add Aws route table
    
    * Link Aws Subnet with the Route table
    
    * Add SSH and Http security groups
    
    * Add Aws Key-Pair
    
    * Add Aws Ubuntu 22.04 AMI Lookup
    
    * Add Aws Ec2 Instance
    
    * Update tfstate
    
    * Fix aws security groups access
    
    Co-authored-by: Pedro Arenas <pedroars99@icloud.com>

--- aws/main.tf
+++ aws/main.tf
@@ -28,4 +28,143 @@ resource "aws_vpc" "tch_vpc" {
     Name = "tch"
   }
 }
+
+resource "aws_subnet" "tch_subnet" {
+  vpc_id                  = aws_vpc.tch_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "tch"
+  }
+}
+
+resource "aws_internet_gateway" "tch_gateway" {
+  vpc_id = aws_vpc.tch_vpc.id
+
+  tags = {
+    Name = "tch"
+  }
+}
+
+resource "aws_route_table" "tch_route_table" {
+  vpc_id = aws_vpc.tch_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.tch_gateway.id
+  }
+
+  tags = {
+    Name = "tch"
+  }
+}
+
+resource "aws_route_table_association" "tch_rt_association" {
+  subnet_id      = aws_subnet.tch_subnet.id
+  route_table_id = aws_route_table.tch_route_table.id
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.tch_vpc.id
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = aws_vpc.tch_vpc.id
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_http"
+  }
+}
+
+resource "aws_key_pair" "tch_key_pair" {
+  key_name   = "tch_key"
+  public_key = file("~/.ssh/tch_rsa.pub")
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Amazon
+}
+
+resource "aws_network_interface" "tch_ec2_network_interface" {
+  subnet_id   = aws_subnet.tch_subnet.id
+  private_ips = ["10.0.1.4"]
+
+  security_groups = [
+    aws_security_group.allow_ssh.id,
+    aws_security_group.allow_http.id
+  ]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+resource "aws_instance" "tch_ec2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  key_name = aws_key_pair.tch_key_pair.id
+
+  tags = {
+    Name = "tch"
+  }
+
+  network_interface {
+    network_interface_id = aws_network_interface.tch_ec2_network_interface.id
+    device_index         = 0
+  }
+}
 // ---
