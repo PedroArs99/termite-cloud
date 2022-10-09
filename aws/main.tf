21088bc929ddbc4d475a6ddfc77538433297f5e8
Pedro Arenas <pedroars99@icloud.com>
2022-10-09 20:07:35 +0200

    Fix aws security groups access

--- aws/main.tf
+++ aws/main.tf
@@ -75,7 +75,7 @@ resource "aws_security_group" "allow_ssh" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [aws_vpc.tch_vpc.cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -101,7 +101,7 @@ resource "aws_security_group" "allow_http" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = [aws_vpc.tch_vpc.cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
