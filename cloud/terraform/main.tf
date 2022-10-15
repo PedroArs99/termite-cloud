816740728c8df8a15a78b9b71bd80dc8d3d3619e
Pedro Arenas <pedroars99@icloud.com>
2022-10-15 22:35:58 +0200

    Add wildcard route 53 domain

--- cloud/terraform/main.tf
+++ cloud/terraform/main.tf
@@ -140,9 +140,17 @@ resource "aws_route53_zone" "termite_cloud" {
   }
 }
 
-resource "aws_route53_record" "termite_cloud" {
+resource "aws_route53_record" "termite_cloud_root" {
   zone_id = aws_route53_zone.termite_cloud.zone_id
   name    = "termite.cloud"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.tch_ec2.public_ip]
+}
+
+resource "aws_route53_record" "termite_cloud_wildcard" {
+  zone_id = aws_route53_zone.termite_cloud.zone_id
+  name    = "*.termite.cloud"
   type    = "A"
   ttl     = 300
   records = [aws_instance.tch_ec2.public_ip]
