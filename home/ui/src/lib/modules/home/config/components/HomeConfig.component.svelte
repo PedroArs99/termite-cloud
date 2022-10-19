c934940780e79a4c33549053ef348abdfad239c0
Pedro Arenas <pedroars99@icloud.com>
2022-10-19 19:20:49 +0200

    Add /config route

---
+++ home/ui/src/lib/modules/home/config/components/HomeConfig.component.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	let config = {
+		bridgeState: 'offline'
+	};
+</script>
+
+<div class="card">
+	<div class="card-body">
+		<div class="property-wrapper">
+			<span>Bridge state</span>
+			{#if config.bridgeState === 'online'}
+				<span class="badge badge-success badge-outline">Online</span>
+			{:else}
+				<div class="badge badge-error badge-outline">Offline</div>
+			{/if}
+		</div>
+	</div>
+</div>
+
+<style class="postcss">
+	.property-wrapper {
+		@apply flex justify-between;
+	}
+</style>
---
+++ home/ui/src/routes/config/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import HomeConfig from "$lib/modules/home/config/components/HomeConfig.component.svelte";
+</script>
+
+<HomeConfig />
