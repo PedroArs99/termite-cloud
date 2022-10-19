6004db229a6da6d831fd0522a97dfffc447fa4dc
Pedro Arenas <pedroars99@icloud.com>
2022-10-19 18:51:51 +0200

    Create Home and Device Modules

--- home/api/src/modules/home/home.module.ts
+++ home/api/src/modules/home/home.module.ts
@@ -1,7 +1,13 @@
 import { Module } from "@nestjs/common";
+import { HomeConfigModule } from "./config/home-config/home-config.module";
+import { HomeDeviceModule } from "./device/home-device/home-device.module";
 import { HomeStateModule } from "./state/home.state.module";
 
 @Module({
-    imports: [HomeStateModule]
+    imports: [
+        HomeConfigModule,
+        HomeDeviceModule,
+        HomeStateModule
+    ]
 })
 export class HomeModule{}
