0a10f97097554cadc61832849ca040efb1c5bf6b
Pedro Arenas <pedroars99@icloud.com>
2022-10-19 19:39:56 +0200

    Implement HomeConfig Store

--- home/ui/src/lib/modules/home/config/components/HomeConfig.component.svelte
+++ home/ui/src/lib/modules/home/config/components/HomeConfig.component.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
-	let config = {
-		bridgeState: 'offline'
-	};
+	import { onMount } from 'svelte';
+	import { fetchHomeConfig, homeConfig } from '../HomeConfig.store';
+
+	onMount(() => fetchHomeConfig);
 </script>
 
 <div class="card">
 	<div class="card-body">
 		<div class="property-wrapper">
 			<span>Bridge state</span>
-			{#if config.bridgeState === 'online'}
+			{#if $homeConfig.bridgeState === 'online'}
 				<span class="badge badge-success badge-outline">Online</span>
 			{:else}
 				<div class="badge badge-error badge-outline">Offline</div>
