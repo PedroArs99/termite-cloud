e240813b9fe2e934d764d826cd16e2151523d9c2
Pedro Arenas <pedroars99@icloud.com>
2022-10-24 20:46:21 +0200

    Dispatch GetAllDevicesQuery on /devices

--- home/api/src/modules/home/device/adapters/rest/home-devices-rest.controller.ts
+++ home/api/src/modules/home/device/adapters/rest/home-devices-rest.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get } from "@nestjs/common";
+import { QueryBus } from "@nestjs/cqrs";
+import { GetAllDevicesQuery } from "../../application/queries/GetAllDevices.query";
 import { DeviceDto } from "./models/device-dto.model";
 
 @Controller("/devices")
 export class HomeDevicesRestController {
+    constructor(private queryBus: QueryBus){}
 
     @Get()
     async getAllDevices(): Promise<Array<DeviceDto>>{
-        return await new Array<DeviceDto>();
+        return this.queryBus.execute(new GetAllDevicesQuery())
     }
 }
--- home/api/src/modules/home/device/home-device.module.ts
+++ home/api/src/modules/home/device/home-device.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { CqrsModule } from "@nestjs/cqrs";
 import { HomeDevicesMqttController } from "./adapters/mqtt/home-devices-mqtt.controller";
 import { InMemoryDeviceRepository } from "./adapters/persistence/InMemoryDeviceRepository.service";
+import { HomeDevicesRestController } from "./adapters/rest/home-devices-rest.controller";
 import { UpsertDevicesHandler } from "./application/commands/upsert-devices.handler";
 import { GetAllDevicesHandler } from "./application/queries/GetAllDevices.handler";
 
@@ -11,7 +12,8 @@ const handlers = [
 ]
 @Module({
     controllers: [
-        HomeDevicesMqttController
+        HomeDevicesMqttController,
+        HomeDevicesRestController,
     ],
     imports: [
         CqrsModule
