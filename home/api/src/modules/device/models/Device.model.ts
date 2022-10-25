fe11431d48275c1150d339429e4217b7642eea5c
Pedro Arenas <pedroars99@icloud.com>
2022-10-25 22:56:19 +0200

    Add domain event triggers

--- home/api/src/modules/device/models/Device.model.ts
+++ home/api/src/modules/device/models/Device.model.ts
@@ -1,21 +1,31 @@
+import { DomainObject } from "src/modules/common/DomainObject.model";
 import { DeviceState } from "./Device-State.model";
+import { DeviceEvent } from "./Device.event";
 
-export class Device {
+export class Device extends DomainObject {
     private constructor(
       readonly friendlyName: string,
       readonly state: DeviceState
-    ) {}
+    ) {
+      super()
+    }
 
     updateState(newState: DeviceState): Device {
-      return new Device(this.friendlyName, newState)
+      const device = new Device(this.friendlyName, newState);
+      device.addEvent(new DeviceEvent())
+
+      return device
     }
 
     toString(): string {
       return JSON.stringify(this);
     }
   
-    static create(friendlyName: string, state: DeviceState): Device{
-      return new Device(friendlyName, state);
+    static create(friendlyName: string, state: DeviceState): Device {
+      const device = new Device(friendlyName, state);
+      device.addEvent(new DeviceEvent())
+
+      return device
     }
   }
   
