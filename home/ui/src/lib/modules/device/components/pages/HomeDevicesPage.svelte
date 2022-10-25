4405530261f66cf0c1e26360a9ddfe7bbd02b94e
Pedro Arenas <pedroars99@icloud.com>
2022-10-25 21:43:07 +0200

    Cleanup Devices module

--- home/ui/src/lib/modules/device/components/pages/HomeDevicesPage.svelte
+++ home/ui/src/lib/modules/device/components/pages/HomeDevicesPage.svelte
@@ -1,29 +1,4 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import type {Device as IDevice} from '../../models/device/Device.model'
-	import { fetchHomeState, homeState } from '../../stores/home.state.store';
-	import Device from '../devices/Device.svelte';
-
-	let devices: IDevice[];
-
-	$: devices = $homeState.devices;
-
-	onMount(() => fetchHomeState());
 </script>
 
-<div id="device-list">
-	{#each devices as device}
-		<Device {device} />
-	{/each}
-</div>
-
-<style lang="scss">
-	#device-list {
-		display: flex;
-		justify-content: space-between;
-
-		@media (min-width: 640px) {
-			justify-content: start;
-		}
-	}
-</style>
+<h1>Here Come the list of devices</h1>
--- home/ui/src/lib/modules/device/models/device/Device.model.ts
+++ home/ui/src/lib/modules/device/models/device/Device.model.ts
@@ -2,7 +2,5 @@ import type { DeviceState } from "./DeviceState.model";
 
 export interface Device {
     friendlyName: string;
-    ieeeAddress: string;
-    type: string;
-    state: DeviceState | "undefined";
+    state: DeviceState;
 } 
