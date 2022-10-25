6bd919ec7c40ca81258eda655fc6d7bacfcc991c
Pedro Arenas <pedroars99@icloud.com>
2022-10-25 20:35:09 +0200

    Implement DeviceService

---
+++ home/api/src/modules/device/application/ports/DeviceService.port.ts
@@ -0,0 +1,3 @@
+export interface DeviceService {
+    refreshState(friendlyName: string): void
+}
--- home/api/src/modules/device/home-device.module.ts
+++ home/api/src/modules/device/home-device.module.ts
@@ -1,31 +1,40 @@
-import { Module } from "@nestjs/common";
-import { CqrsModule } from "@nestjs/cqrs";
-import { HomeDevicesMqttController } from "./adapters/mqtt/home-devices-mqtt.controller";
-import { InMemoryDeviceRepository } from "./adapters/persistence/InMemoryDeviceRepository.service";
-import { HomeDevicesRestController } from "./adapters/rest/home-devices-rest.controller";
-import { RegisterDevicesHandler } from "./application/commands/register/register-devices.handler";
-import { UpdateDeviceStateHandler } from "./application/commands/updateState/updateState.handler";
-import { GetAllDevicesHandler } from "./application/queries/GetAllDevices.handler";
+import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { HomeDevicesMqttController } from './adapters/mqtt/home-devices-mqtt.controller';
+import { DeviceServiceImpl } from './adapters/mqtt/home-devices-mqtt.service';
+import { InMemoryDeviceRepository } from './adapters/persistence/InMemoryDeviceRepository.service';
+import { HomeDevicesRestController } from './adapters/rest/home-devices-rest.controller';
+import { RegisterDevicesHandler } from './application/commands/register/register-devices.handler';
+import { UpdateDeviceStateHandler } from './application/commands/updateState/updateState.handler';
+import { GetAllDevicesHandler } from './application/queries/GetAllDevices.handler';
 
 const handlers = [
-    GetAllDevicesHandler,
-    RegisterDevicesHandler,
-    UpdateDeviceStateHandler
-]
+  GetAllDevicesHandler,
+  RegisterDevicesHandler,
+  UpdateDeviceStateHandler,
+];
 @Module({
-    controllers: [
-        HomeDevicesMqttController,
-        HomeDevicesRestController,
-    ],
-    imports: [
-        CqrsModule
-    ],
-    providers: [
-        ...handlers,
-        {
-            provide: 'DeviceRepository',
-            useClass: InMemoryDeviceRepository
-        }
-    ]
+  controllers: [HomeDevicesMqttController, HomeDevicesRestController],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'MQTT_client',
+        transport: Transport.MQTT,
+      },
+    ]),
+    CqrsModule,
+  ],
+  providers: [
+    ...handlers,
+    {
+      provide: 'DeviceRepository',
+      useClass: InMemoryDeviceRepository,
+    },
+    {
+      provide: 'DeviceService',
+      useClass: DeviceServiceImpl,
+    },
+  ],
 })
 export class HomeDeviceModule {}
