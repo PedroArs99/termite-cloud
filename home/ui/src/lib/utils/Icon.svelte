12b2ce3db03e144f274c879456df10afcd7a4545
Pedro Arenas <pedroars99@icloud.com>
2022-10-25 22:01:52 +0200

    Remove Signal Icon

--- home/ui/src/lib/utils/Icon.svelte
+++ home/ui/src/lib/utils/Icon.svelte
@@ -2,17 +2,15 @@
 	import Fa from 'svelte-fa';
 	import { 
         faLightbulb,
-        faSignal
     } from '@fortawesome/free-solid-svg-icons';
 
     export let clazz: string = "";
     export let color: string | undefined = undefined;
-    export let icon:  "lightbulb" | "signal";
+    export let icon:  "lightbulb";
     export let size: string = "lg";
 
     let icons = {
         "lightbulb": faLightbulb,
-        "signal": faSignal
     }
 </script>
 
