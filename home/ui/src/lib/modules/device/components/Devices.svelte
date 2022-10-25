19dd137199d5985b65e8cb3f7b324e3e2b92e347
Pedro Arenas <pedroars99@icloud.com>
2022-10-25 21:49:18 +0200

    Add Devices Component

---
+++ home/ui/src/lib/modules/device/components/Devices.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { fetchDevices } from '../stores/device.store';
+	import { devices } from '../stores/device.store';
+
+	onMount(() => fetchDevices());
+</script>
+
+{#each $devices as device}
+	<div class="card">
+		<div class="card-body">
+			<h1 class="text-center">{device.friendlyName}</h1>
+		</div>
+	</div>
+{/each}
--- home/ui/src/lib/modules/device/components/pages/HomeDevicesPage.svelte
+++
@@ -1,4 +0,0 @@
-<script lang="ts">
-</script>
-
-<h1>Here Come the list of devices</h1>
