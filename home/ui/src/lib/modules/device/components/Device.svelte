16265c12731f6c23a9546d78af8bac52bab33622
Pedro Arenas <pedroars99@icloud.com>
2022-10-25 22:11:47 +0200

    Introduce Device component

---
+++ home/ui/src/lib/modules/device/components/Device.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { Device } from "../models/device/Device.model";
+    import Icon from "$lib/utils/Icon.svelte"
+
+    export let device: Device;
+</script>
+
+<div class="card">
+    <div class="card-body">
+        <Icon clazz="border rounded-full p-3" icon="lightbulb" size="5x" />
+        <h1 class="text-center">{device.friendlyName}</h1>
+    </div>
+</div>
+
--- home/ui/src/lib/modules/device/components/Devices.svelte
+++ home/ui/src/lib/modules/device/components/Devices.svelte
@@ -1,19 +1,14 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { devices, fetchDevices } from '../stores/device.store';
-	import Icon from '$lib/utils/Icon.svelte';
+    import Device from './Device.svelte'
 
 	onMount(() => fetchDevices());
 </script>
 
 <div id="device-list">
 	{#each $devices as device}
-		<div class="card">
-			<div class="card-body">
-				<Icon clazz="border rounded-full p-3" icon="lightbulb" size="5x" />
-				<h1 class="text-center">{device.friendlyName}</h1>
-			</div>
-		</div>
+		<Device {device} />
 	{/each}
 </div>
 
