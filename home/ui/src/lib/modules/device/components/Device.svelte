088d975f625303716fe8033786ca495872bf4963
Pedro Arenas <pedroars99@icloud.com>
2022-10-26 18:46:37 +0200

    Add Device in/out animations

--- home/ui/src/lib/modules/device/components/Device.svelte
+++ home/ui/src/lib/modules/device/components/Device.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 	import type { Device } from "../models/device/Device.model";
     import Icon from "$lib/utils/Icon.svelte"
+	import { fade, fly } from "svelte/transition";
 
     export let device: Device;
 
     $: color = device.state.power === "ON" ? "#EFC070": "#333333";
 </script>
 
-<div class="card">
+<div in:fade out:fly class="card">
     <div class="card-body">
         <Icon clazz="border rounded-full p-3" icon="lightbulb" size="5x" {color} />
         <h1 class="text-center">{device.friendlyName.replace("_", " ")}</h1>
