5bd09895dd0ce8f8a930211956d0b7428cd4a738
Pedro Arenas <pedroars99@icloud.com>
2022-10-27 22:39:09 +0200

    Add publishDeviceState method

--- home/api/src/modules/device/adapters/mqtt/home-devices-mqtt.service.ts
+++ home/api/src/modules/device/adapters/mqtt/home-devices-mqtt.service.ts
@@ -3,6 +3,8 @@ import { DeviceService } from '../../application/ports/DeviceService.port';
 import { ConfigService } from '@nestjs/config';
 import { connectAsync } from 'async-mqtt';
 import { MqttClient } from 'mqtt';
+import { Device } from '../../models/Device.model';
+import { deviceStateDtoFromDomain } from './models/device-state-dto.model';
 
 @Injectable()
 export class DeviceServiceImpl implements DeviceService {
@@ -10,14 +12,22 @@ export class DeviceServiceImpl implements DeviceService {
 
   async refreshState(friendlyName: string): Promise<void> {
     const mqttClient = await this.connectClient();
-    mqttClient.publish(`zigbee2mqtt/${friendlyName}/get`, '{ "state": "" }')
-    mqttClient.end()
+    mqttClient.publish(`zigbee2mqtt/${friendlyName}/get`, '{ "state": "" }');
+    mqttClient.end();
   }
 
+  async publishDeviceState(device: Device): Promise<void> {
+    const mqttClient = await this.connectClient();
+
+    const deviceStateDto = deviceStateDtoFromDomain(device.state);
+    mqttClient.publish(
+      `zigbee2mqtt/${device.friendlyName}/set`,
+      JSON.stringify(deviceStateDto),
+    );
+  }
 
   private async connectClient(): Promise<MqttClient> {
-    const mqttUrl = this.configService.get("MQTT_URL"); 
+    const mqttUrl = this.configService.get('MQTT_URL');
     return await connectAsync(mqttUrl);
   }
-
 }
