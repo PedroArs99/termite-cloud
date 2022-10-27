d0532754af987f68a07e6f420e4b9bab056df6bb
Pedro Arenas <pedroars99@icloud.com>
2022-10-27 23:04:48 +0200

    Add toggle state method

--- home/ui/src/lib/modules/device/models/device/Device.model.ts
+++ home/ui/src/lib/modules/device/models/device/Device.model.ts
@@ -3,4 +3,23 @@ import type { DeviceState } from "./DeviceState.model";
 export interface Device {
     friendlyName: string;
     state: DeviceState;
-} 
+}
+
+export class DeviceImpl implements Device {
+    readonly friendlyName: string;
+    readonly state: DeviceState;
+
+    constructor(friendlyName: string, state: DeviceState){
+        this.friendlyName = friendlyName;
+        this.state = state;
+    }
+
+    toggleDeviceState(): Device {
+        const toggledState = {
+            ...this.state,
+            power: this.state.power === "ON" ? "OFF": "ON"
+        }
+
+        return new DeviceImpl(this.friendlyName, toggledState)
+    };
+}
