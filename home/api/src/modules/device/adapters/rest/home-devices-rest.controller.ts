13767009b14739394baa28b11a2c026badf9cb21
Pedro Arenas <pedroars99@icloud.com>
2022-10-27 22:22:09 +0200

    Add Put /friendlyName endpoint

--- home/api/src/modules/device/adapters/rest/home-devices-rest.controller.ts
+++ home/api/src/modules/device/adapters/rest/home-devices-rest.controller.ts
@@ -1,14 +1,34 @@
-import { Controller, Get } from "@nestjs/common";
-import { QueryBus } from "@nestjs/cqrs";
-import { GetAllDevicesQuery } from "../../application/queries/GetAllDevices.query";
-import { DeviceDto } from "./models/device-dto.model";
+import { Body, Controller, Get, Param, Put } from '@nestjs/common';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { UpdateDeviceStateCommand } from '../../application/commands/updateState/updateState.command';
+import { GetAllDevicesQuery } from '../../application/queries/GetAllDevices.query';
+import { Device } from '../../models/Device.model';
+import { DeviceDto } from './models/device-dto.model';
+import {
+  DeviceStateDto,
+  deviceStateDtoToDomain,
+} from './models/device-state-dto.model';
 
-@Controller("/devices")
+@Controller('/devices')
 export class HomeDevicesRestController {
-    constructor(private queryBus: QueryBus){}
+  constructor(private commandBus: CommandBus, private queryBus: QueryBus) {}
 
-    @Get()
-    async getAllDevices(): Promise<Array<DeviceDto>>{
-        return this.queryBus.execute(new GetAllDevicesQuery())
-    }
+  @Get()
+  async getAllDevices(): Promise<Array<DeviceDto>> {
+    return this.queryBus.execute(new GetAllDevicesQuery());
+  }
+
+  @Put('/{friendlyName}')
+  async updateDeviceState(
+    @Param('friendlyName') friendlyName: string,
+    @Body() newState: DeviceStateDto,
+  ): Promise<DeviceDto> {
+    const command = new UpdateDeviceStateCommand(
+      friendlyName,
+      deviceStateDtoToDomain(newState),
+      true,
+    );
+
+    return this.commandBus.execute(command);
+  }
 }
--- home/api/src/modules/device/adapters/rest/models/device-state-dto.model.ts
+++ home/api/src/modules/device/adapters/rest/models/device-state-dto.model.ts
@@ -1,3 +1,11 @@
+import { DeviceState } from "src/modules/device/models/Device-State.model";
+
 export interface DeviceStateDto {
     power: "ON" | "OFF";
+}
+
+export function deviceStateDtoToDomain(dto: DeviceStateDto): DeviceState {
+    return DeviceState.create(
+        dto.power
+    )
 }
