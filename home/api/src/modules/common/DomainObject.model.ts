f6094abebf0e68c90c068ca0e9b424a2aeb9a7f3
Pedro Arenas <pedroars99@icloud.com>
2022-10-27 22:27:06 +0200

    Trigger DeviceMqttContextUpdated on state update

--- home/api/src/modules/common/DomainObject.model.ts
+++ home/api/src/modules/common/DomainObject.model.ts
@@ -3,7 +3,7 @@ import { Event } from './Event.model';
 export class DomainObject {
     private _events = new Array<Event>();
     
-    addEvent(event: any): void {
+    addEvent(event: Event): void {
         this._events.push(event);
     }
 
