fccdbffb9d2e6953d7a99caba00370b67c8f57d4
Pedro Arenas <pedroars99@icloud.com>
2022-10-30 19:40:41 +0100

    Add sun & circle icons

--- home/ui/src/lib/utils/Icon.svelte
+++ home/ui/src/lib/utils/Icon.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
 	import Fa from 'svelte-fa';
-	import { 
-        faLightbulb,
-    } from '@fortawesome/free-solid-svg-icons';
+	import { faLightbulb, faSun } from '@fortawesome/free-solid-svg-icons';
+	import { faCircle } from '@fortawesome/free-regular-svg-icons';
 
-    export let clazz: string = "";
-    export let color: string | undefined = undefined;
-    export let icon:  "lightbulb";
-    export let size: string = "lg";
+	export let clazz: string = '';
+	export let color: string | undefined = undefined;
+	export let icon: 'circle' | 'lightbulb' | 'sun';
+	export let size: string = 'lg';
 
-    let icons = {
-        "lightbulb": faLightbulb,
-    }
+	let icons = {
+		circle: faCircle,
+		lightbulb: faLightbulb,
+		sun: faSun
+	};
 </script>
 
 <Fa class={clazz} {color} icon={icons[icon]} {size} />
