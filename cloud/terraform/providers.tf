03db7b9ae7c7829a3dfefc80302550aca316ac52
Pedro Arenas <pedroars99@icloud.com>
2022-10-30 21:31:47 +0100

    Use S3 Bucket as terraform backend

--- cloud/terraform/providers.tf
+++ cloud/terraform/providers.tf
@@ -5,6 +5,12 @@ terraform {
       version = "4.33.0"
     }
   }
+
+  backend "s3" {
+    bucket = "termite-cloud-terraform-backend"
+    key = "terraform.tfstate"
+    region = "eu-central-1"
+  }
 }
 
 provider "aws" {
