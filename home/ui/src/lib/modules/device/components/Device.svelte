0f8779eb41d035b39320193ae51caed8b89b7a5a
Pedro Arenas <pedroars99@icloud.com>
2022-10-30 19:14:33 +0100

    Add brightness slider

--- home/ui/src/lib/modules/device/components/Device.svelte
+++ home/ui/src/lib/modules/device/components/Device.svelte
@@ -2,7 +2,7 @@
 	import type { Device } from '../models/device/Device.model';
 	import Icon from '$lib/utils/Icon.svelte';
 	import { fade, fly } from 'svelte/transition';
-	import { toggleDeviceState } from '../stores/device.store';
+	import { toggleDeviceState, updateDeviceBrightness } from '../stores/device.store';
 
 	export let device: Device;
 
@@ -10,12 +10,19 @@
 		toggleDeviceState(device);
 	}
 
+	function onBrightnessChange(event: Event) {
+		const target = event.target as HTMLInputElement;
+		const brightness = +target.value;
+
+		updateDeviceBrightness(device, brightness);
+	}
+
 	$: color = device.state.power === 'ON' ? '#EFC070' : '#333333';
 </script>
 
 <div in:fade out:fly class="card">
 	<div class="card-body">
-		<div on:click={() => onDeviceClick()}>
+		<div class="icon-wrapper" on:click={() => onDeviceClick()}>
 			<Icon
 				clazz="border rounded-full p-3 hover:cursor-pointer hover:bg-primary"
 				icon="lightbulb"
@@ -23,6 +30,20 @@
 				{color}
 			/>
 		</div>
+		<input
+			type="range"
+			min="0"
+			max="254"
+			value={device.state.brightness}
+			class="range range-xs"
+			on:change={(e) => onBrightnessChange(e)}
+		/>
 		<h1 class="text-center">{device.friendlyName.replace('_', ' ')}</h1>
 	</div>
 </div>
+
+<style lang="scss">
+	.icon-wrapper {
+		display: contents;
+	}
+</style>
