dd66fd132eafd45c1b05055994e35238fa6a9785
Pedro Arenas <pedroars99@icloud.com>
2022-11-06 22:19:56 +0100

    Add TemperatureSlider

---
+++ home/ui/src/lib/modules/device/components/TemperatureSlider.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	import Icon from '$lib/utils/Icon.svelte';
+
+	export let value: number = 0;
+
+	const dispatch = createEventDispatcher();
+
+	function onTemperatureChange(e: Event) {
+		const target = e.target as HTMLInputElement;
+		const temperature = +target.value;
+
+		dispatch('change', temperature);
+	}
+</script>
+
+<div class="flex gap-1 my-3">
+	<Icon icon="cold" size="md"/>
+	<input
+		type="range"
+		min="250"
+		max="454"
+		{value}
+		class="range range-xs"
+		on:change={(e) => onTemperatureChange(e)}
+	/>
+    <Icon icon="warm" size="md"/>
+</div>
