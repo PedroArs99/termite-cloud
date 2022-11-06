53c3c65d6514b3a853e63e3cecbaa8305b4c11c2
Pedro Arenas <pedroars99@icloud.com>
2022-11-06 22:00:13 +0100

    Track Color temperature

--- home/api/src/modules/device/adapters/mqtt/models/device-state-dto.model.ts
+++ home/api/src/modules/device/adapters/mqtt/models/device-state-dto.model.ts
@@ -1,17 +1,21 @@
-import { DeviceState } from "src/modules/device/models/Device-State.model";
+import { DeviceState } from 'src/modules/device/models/Device-State.model';
 
 export interface DeviceStateDto {
-    state: "ON" | "OFF"
-    brightness: number
+  brightness: number;
+  state: 'ON' | 'OFF';
+  color_temp: number;
 }
 
 export function deviceStateToDomain(dto: DeviceStateDto): DeviceState {
-    return DeviceState.create(dto.state, dto.brightness);
+  return DeviceState.create(dto.state, dto.brightness, dto.color_temp);
 }
 
-export function deviceStateDtoFromDomain(deviceState: DeviceState): DeviceStateDto {
-    return {
-        state: deviceState.power,
-        brightness: deviceState.brightness
-    }
+export function deviceStateDtoFromDomain(
+  deviceState: DeviceState,
+): DeviceStateDto {
+  return {
+    state: deviceState.power,
+    brightness: deviceState.brightness,
+    color_temp: deviceState.colorTemperature
+  };
 }
--- home/api/src/modules/device/models/Device-State.model.ts
+++ home/api/src/modules/device/models/Device-State.model.ts
@@ -1,13 +1,15 @@
 export class DeviceState {
     private constructor(
         readonly power: "ON" | "OFF",
-        readonly brightness: number
+        readonly brightness: number,
+        readonly colorTemperature: number
     ){}
 
     static create(
         power: "ON" | "OFF",
-        brightness: number
+        brightness: number,
+        colorTemperature: number
     ): DeviceState {
-        return new DeviceState(power, brightness)
+        return new DeviceState(power, brightness, colorTemperature)
     }
 }
