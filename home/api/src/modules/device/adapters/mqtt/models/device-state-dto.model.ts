ca708c5c6d5833b1658171730ee43176920ec6c4
Pedro Arenas <pedroars99@icloud.com>
2022-11-07 22:38:13 +0100

    Map XY color to domain

--- home/api/src/modules/device/adapters/mqtt/models/device-state-dto.model.ts
+++ home/api/src/modules/device/adapters/mqtt/models/device-state-dto.model.ts
@@ -1,21 +1,56 @@
 import { DeviceState } from 'src/modules/device/models/Device-State.model';
+import ColorConverter from 'cie-rgb-color-converter';
 
 export interface DeviceStateDto {
   brightness: number;
   state: 'ON' | 'OFF';
   color_temp: number;
+  color: { x: number; y: number };
 }
 
 export function deviceStateToDomain(dto: DeviceStateDto): DeviceState {
-  return DeviceState.create(dto.state, dto.brightness, dto.color_temp);
+  const rgb: { r: number; g: number; b: number } = ColorConverter.xyBriToRgb(
+    dto.color.x,
+    dto.color.y,
+    dto.brightness,
+  );
+  const hex = rgbToHex(rgb);
+
+  return DeviceState.create(dto.state, dto.brightness, dto.color_temp, hex);
 }
 
 export function deviceStateDtoFromDomain(
   deviceState: DeviceState,
 ): DeviceStateDto {
+  const { r, g, b } = hexToRgb(deviceState.color);
+  const { x, y } = ColorConverter.rgbToXy(r, g, b);
+
   return {
     state: deviceState.power,
     brightness: deviceState.brightness,
-    color_temp: deviceState.colorTemperature
+    color_temp: deviceState.colorTemperature,
+    color: {
+      x,
+      y,
+    },
+  };
+}
+
+function valueToHex(c: number): string {
+  return c.toString(16);
+}
+
+function rgbToHex({ r, g, b }: any): string {
+  const value = valueToHex(r) + valueToHex(g) + valueToHex(b);
+
+  return `#${value.toUpperCase()}`;
+}
+
+function hexToRgb(hex: string): { r: number; g: number; b: number } {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16),
   };
 }
--- home/api/src/modules/device/models/Device-State.model.ts
+++ home/api/src/modules/device/models/Device-State.model.ts
@@ -2,14 +2,16 @@ export class DeviceState {
     private constructor(
         readonly power: "ON" | "OFF",
         readonly brightness: number,
-        readonly colorTemperature: number
+        readonly colorTemperature: number,
+        readonly color: string
     ){}
 
     static create(
         power: "ON" | "OFF",
         brightness: number,
-        colorTemperature: number
+        colorTemperature: number,
+        color: string
     ): DeviceState {
-        return new DeviceState(power, brightness, colorTemperature)
+        return new DeviceState(power, brightness, colorTemperature, color)
     }
 }
