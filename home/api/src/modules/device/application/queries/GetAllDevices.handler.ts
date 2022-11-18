4ca6ae9ce2bb90180b7296a4ef661d506c0da06b
Pedro Arenas <pedroars99@icloud.com>
2022-11-18 19:53:16 +0100

    Sort devices by friendly name

--- home/api/src/modules/device/application/queries/GetAllDevices.handler.ts
+++ home/api/src/modules/device/application/queries/GetAllDevices.handler.ts
@@ -6,11 +6,19 @@ import { GetAllDevicesQuery } from './GetAllDevices.query';
 
 @QueryHandler(GetAllDevicesQuery)
 export class GetAllDevicesHandler
-  implements IQueryHandler<GetAllDevicesQuery, Array<Device>> {
-    constructor(@Inject("DeviceRepository") private deviceRepo: DeviceRepository){}
+  implements IQueryHandler<GetAllDevicesQuery, Array<Device>>
+{
+  constructor(
+    @Inject('DeviceRepository') private deviceRepo: DeviceRepository,
+  ) {}
 
-    async execute(query: GetAllDevicesQuery): Promise<Device[]> {
-        return this.deviceRepo.findAll()
-    }
-    
+  async execute(query: GetAllDevicesQuery): Promise<Device[]> {
+    const results = await this.deviceRepo.findAll();
+
+    const sortedResults = results.sort((d1, d2) =>
+      d1.friendlyName.localeCompare(d2.friendlyName),
+    );
+
+    return sortedResults;
   }
+}
