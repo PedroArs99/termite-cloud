98abf1dee31b724fdc9a859b82cadea08f6d9e4f
Pedro Arenas <pedroars99@icloud.com>
2022-11-20 12:51:13 +0100

    Improve API exception logging

--- home/api/src/modules/device/adapters/mqtt/models/device-state-dto.model.ts
+++ home/api/src/modules/device/adapters/mqtt/models/device-state-dto.model.ts
@@ -48,11 +48,15 @@ function rgbToHex({ r, g, b }: any): string {
 }
 
 function hexToRgb(hex: string): { r: number; g: number; b: number } {
-  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  Logger.error(`Parsing ${hex} to rgg with regex: ${result.toString()}`)
-  return {
-    r: parseInt(result[1], 16),
-    g: parseInt(result[2], 16),
-    b: parseInt(result[3], 16),
-  };
+  try {
+    let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+    return {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16),
+    };
+  } catch (e) {
+    Logger.error(`Type Error when casting Hex-to-Rgb: ${hex}`);
+  }
 }
